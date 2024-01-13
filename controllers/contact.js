afce423295b7a916e0557b6f6e020bbc47248357
Dmytro Sayko <saykodmytro@icloud.com>
2024-01-13 19:18:49 +0200

    up

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,26 +1,19 @@
 const Contact = require("../models/contact");
+const { ctrlWrapper } = require("../utils/index");
 
 const listContacts = async (req, res, next) => {
-  try {
-    const contacts = await Contact.find();
-    res.send(contacts);
-  } catch (error) {
-    next(error);
-  }
+  const contacts = await Contact.find();
+  res.send(contacts);
 };
 
 const getContactById = async (req, res, next) => {
   const { id } = req.params;
 
-  try {
-    const contact = await Contact.findById(id);
-    if (contact === null) {
-      return res.status(404).send("Contact not found");
-    }
-    res.send(contact);
-  } catch (error) {
-    next(error);
+  const contact = await Contact.findById(id);
+  if (contact === null) {
+    return res.status(404).send("Contact not found");
   }
+  res.send(contact);
 };
 
 const addContact = async (req, res, next) => {
@@ -30,27 +23,18 @@ const addContact = async (req, res, next) => {
     phone: req.body.phone,
   };
 
-  try {
-    const result = await Contact.create(contact);
-
-    res.status(201).send(result);
-  } catch (error) {
-    next(error);
-  }
+  const result = await Contact.create(contact);
+  res.status(201).send(result);
 };
 
 const removeContact = async (req, res, next) => {
   const { id } = req.params;
 
-  try {
-    const contact = await Contact.findByIdAndDelete(id);
-    if (contact === null) {
-      return res.status(404).send("Contact not found");
-    }
-    res.send(contact);
-  } catch (error) {
-    next(error);
+  const contact = await Contact.findByIdAndDelete(id);
+  if (contact === null) {
+    return res.status(404).send("Contact not found");
   }
+  res.send(contact);
 };
 
 const updateContact = async (req, res, next) => {
@@ -61,46 +45,38 @@ const updateContact = async (req, res, next) => {
     phone: req.body.phone,
   };
 
-  try {
-    const result = await Contact.findByIdAndUpdate(id, contact, { new: true });
+  const result = await Contact.findByIdAndUpdate(id, contact, { new: true });
 
-    if (result === null) {
-      return res.status(404).send("Book not found");
-    }
-
-    res.send(result);
-  } catch (error) {
-    next(error);
+  if (result === null) {
+    return res.status(404).send("Contact not found");
   }
+
+  res.send(result);
 };
 
 const changeContactFavorite = async (req, res, next) => {
   const { id } = req.params;
 
-  try {
-    const result = await Contact.findByIdAndUpdate(
-      id,
-      {
-        favorite: req.body.favorite,
-      },
-      { new: true }
-    );
+  const result = await Contact.findByIdAndUpdate(
+    id,
+    {
+      favorite: req.body.favorite,
+    },
+    { new: true }
+  );
 
-    if (result === null) {
-      return res.status(404).send("Book not found");
-    }
-
-    res.send(result);
-  } catch (error) {
-    next(error);
+  if (result === null) {
+    return res.status(404).send("Contact not found");
   }
+
+  res.send(result);
 };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-  changeContactFavorite,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  removeContact: ctrlWrapper(removeContact),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  changeContactFavorite: ctrlWrapper(changeContactFavorite),
 };
