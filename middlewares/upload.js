cb97e078ba58cbc7d07e17f26343dc9caffafd48
Dmytro Sayko <saykodmytro@icloud.com>
2024-02-04 15:21:38 +0200

    up

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,9 +1,10 @@
+const path = require("node:path");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cd) => {
     console.log(file);
-    cd(null);
+    cd(null, path.join(__dirname, "..", "tmp"));
   },
 });
 
